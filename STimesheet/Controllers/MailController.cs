f88f0b897edd2349c3e40b21b87a55fb4e697df5
janani <janani@DESKTOP-NQBR20J>
2023-04-23 15:46:11 +0530

    Add project files.

---
+++ STimesheet/Controllers/MailController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using STimesheet.Models;
+using STimesheet.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STimesheet.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+
+        public MailController(IMailService mailService)
+        {
+            this._mailService = mailService;
+        }
+
+        [HttpPost]
+        [Route("send")]
+        public async Task<IActionResult> SendMail([FromBody] MailRequest request)
+        {
+            try
+            {
+                await _mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+          
+    }
+}
---
+++ STimesheet/Services/MailService.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using STimesheet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STimesheet.Services
+{
+    public class MailService:IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            /*email.Subject = mailRequest.Subject;*/
+            email.Subject = "Reset Password Link..";
+            var builder = new BodyBuilder();
+            var Body = "please click link";
+            /* builder.HtmlBody = mailRequest.Body;*/
+            builder.HtmlBody = Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
